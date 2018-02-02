28fac3d379dc12d4bfa9a18a987f7e803bdd0d14
Analyn Cajocson <analyncajocson@ymail.com>
2018-02-02 11:50:50 +0800

    route for doorkeeper and api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :items
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
+  devise_for :users
+  # resources :items
+  namespace :api do
+    resources :items
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
