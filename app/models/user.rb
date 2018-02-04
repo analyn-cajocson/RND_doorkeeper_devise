c8f336f96b5fd542b849f86c98d9acc675b28887
Analyn Cajocson <analyncajocson@ymail.com>
2018-02-04 11:47:40 +0800

    force logout previous session

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,17 @@ class User < ActiveRecord::Base
   def self.authenticate(email, password)
     user = User.find_for_authentication(email: email)
     user.try(:valid_password?, password) ? user : nil
+    revoke_token(user)
+    user
+  end
+
+  def self.revoke_token(user)
+    if user
+      sessions = OauthAccessToken.where(resource_owner_id: user.id)
+      sessions.each do |s|
+        s.revoked_at = DateTime.now
+        s.save
+      end
+    end
   end
 end
